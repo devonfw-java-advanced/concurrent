d60577b818a0a6f97fedd9010006f7b86efccfc9
Jacek Czastkiewicz <external.jacek.czastkiewicz@kuehne-nagel.com>
2022-09-10 14:28:15 +0200

    Fix exc

--- src/main/java/com/devonfw/java/training/concurrent/service/MathService.java
+++ src/main/java/com/devonfw/java/training/concurrent/service/MathService.java
@@ -3,21 +3,26 @@ package com.devonfw.java.training.concurrent.service;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.concurrent.CompletableFuture;
 
 @Service
 public class MathService {
 
     @Async
-    public CompletableFuture<Double> multiplyAsync(Double a, Double b) throws InterruptedException {
+    public CompletableFuture<Double> multiplyAsync(Double a, Double b) {
         return CompletableFuture.completedFuture(multiply(a, b));
     }
 
-    public Double multiply(Double a, Double b) throws InterruptedException {
-        // multiply takes some time
-        for (int i = 0; i < 3 && !Thread.currentThread().isInterrupted(); i++) {
-            Thread.sleep(1000);
+    public Double multiply(Double a, Double b) {
+
+        Instant now = Instant.now();
+        Instant end = now.plusSeconds(3);
+        while (end.isAfter(now) && !Thread.currentThread().isInterrupted()) {
+            // multiply takes some time
+            now = Instant.now();
         }
+
         return a * b;
     }
 
